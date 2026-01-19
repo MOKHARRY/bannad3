ed2a1b57645cb244dc7a8b9fe6f86719ef4fa6d5
MOKHARRY <110606458+MOKHARRY@users.noreply.github.com>
2026-01-19 16:55:14 +0100

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// Configurable links
-const config = {
-  title: "Minecraft Server | Maintenance",
-  discord: "https://discord.gg/yourserver",
-  vote: "https://minecraft-server-vote-link.com",
-  store: "https://yourstore.com"
-};
